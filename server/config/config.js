12c0bf69d5d9352a1f759c97f115c1cfc574b51f
kodykam <bigachiever@icloud.com>
2025-12-21 22:17:56 -0500

    Updated config.js with new connection string

--- server/config/config.js
+++ server/config/config.js
@@ -4,7 +4,7 @@ const config = {
   port: process.env.PORT || 5000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
   mongoUri:
-    process.env.MONGODB_URI || "mongodb+srv://bigachiever:inCanada31@cluster0.fk33n7t.mongodb.net/Portfolio?retryWrites=true&w=majority&appName=Cluster0"
+    process.env.MONGODB_URI || "mongodb+srv://Kam_Admin:IJYQra3jG8vjX4sB@kodykam.ul5ycz8.mongodb.net/Portfolio?retryWrites=true&w=majority&appName=KodyKam"
 };
 
 export default config;
