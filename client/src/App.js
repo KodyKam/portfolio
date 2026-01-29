cdf838747788539d58364056ccb65fd6da40d9a0
kodykam <bigachiever@icloud.com>
2026-01-28 23:21:27 -0500

    Clean up unused imports, fix ESLint issues, update Profile & ClientDashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 // client/src/App.js
-import React from 'react';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
